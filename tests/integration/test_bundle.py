413236bb83d4dcb63d3540ece9134ace5f0ae611
Paulo Machado <paulo.machado@canonical.com>
2022-10-18 22:45:58 -0300

    initial test

--- tests/integration/test_bundle.py
+++ tests/integration/test_bundle.py
@@ -8,9 +8,17 @@ import logging
 
 from pytest_operator.plugin import OpsTest
 
+from tests.integration.constants import MYSQL_APP, ROUTER_APP, TLS_APP
+
 logger = logging.getLogger(__name__)
 
 
-async def test_placeholder(ops_test: OpsTest) -> None:
-    """A placeholder for an integration test to be implemented."""
-    assert True
+async def test_deploy_bundle(ops_test: OpsTest) -> None:
+    """Deploy bundle."""
+
+    async with ops_test.fast_forward():
+        await ops_test.model.deploy("./releases/latest/mysql-k8s-bundle.yaml")
+
+        await ops_test.model.wait_for_idle(
+            app=[MYSQL_APP, ROUTER_APP, TLS_APP], status="active", timeout=5 * 60
+        )
