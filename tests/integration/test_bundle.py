1307056c896e7eb5bfd5b460c9222ee86f871fe5
Paulo Machado <paulo.machado@canonical.com>
2022-10-19 19:49:38 -0300

    minimal working test

--- tests/integration/test_bundle.py
+++ tests/integration/test_bundle.py
@@ -17,8 +17,10 @@ async def test_deploy_bundle(ops_test: OpsTest) -> None:
     """Deploy bundle."""
 
     async with ops_test.fast_forward():
-        await ops_test.model.deploy("./releases/latest/mysql-k8s-bundle.yaml")
+        await ops_test.model.deploy("./releases/latest/mysql-k8s-bundle.yaml", trust=True)
 
         await ops_test.model.wait_for_idle(
-            app=[MYSQL_APP, ROUTER_APP, TLS_APP], status="active", timeout=5 * 60
+            apps=[MYSQL_APP, TLS_APP], status="active", timeout=5 * 60
         )
+
+        await ops_test.model.wait_for_idle(apps=[ROUTER_APP], status="waiting", timeout=5 * 60)
