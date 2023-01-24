44e3cf740545f7e3e5bea6187c8387fdd6ce8684
Carl Csaposs <carl.csaposs@canonical.com>
2023-01-24 14:12:41 +0000

    Support Ubuntu versions in update_bundle.py (#19)

--- update_bundle.py
+++ update_bundle.py
@@ -3,16 +3,17 @@
 # See LICENSE file for licensing details.
 
 import logging
+import subprocess
+import sys
+from pathlib import Path
+
 import requests
 import yaml
-import sys
-
-from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
 
-def fetch_revision(charm, charm_channel):
+def fetch_revision(charm, charm_channel, ubuntu_version=None):
     """Returns revision number for charm in channel."""
     charm_info = requests.get(
         f"https://api.snapcraft.io/v2/charms/info/{charm}?fields=channel-map"
@@ -20,25 +21,39 @@ def fetch_revision(charm, charm_channel):
 
     [track, risk] = charm_channel.split("/")
 
+    revisions = []
     for channel in charm_info["channel-map"]:
         if channel["channel"]["risk"] == risk and channel["channel"]["track"] == track:
-            return channel["revision"]["revision"]
+            if ubuntu_version is None or ubuntu_version == channel["channel"]["base"]["channel"]:
+                revisions.append(channel["revision"]["revision"])
+    if revisions:
+        # If the charm supports multiple Ubuntu bases, it's possible
+        # that there is a different revision for each base.
+        # Select the latest revision.
+        return max(revisions)
 
-    raise ValueError(f"Revision not found for {charm} on {charm_channel}")
+    raise ValueError(f"Revision not found for {charm} on {charm_channel} for Ubuntu {ubuntu_version}")
+
 
 def update_bundle(bundle_path):
     """Updates a bundle's revision number."""
     bundle_data = yaml.safe_load(Path(bundle_path).read_text())
 
     for applications in bundle_data["applications"]:
+        if series := bundle_data["applications"][applications].get("series"):
+            ubuntu_version = subprocess.check_output(
+                ["ubuntu-distro-info", "--series", series, "--release"], encoding="utf-8"
+            ).split(" ")[0]
+        else:
+            ubuntu_version = None
         bundle_data["applications"][applications]["revision"] = fetch_revision(
-            applications, bundle_data["applications"][applications]["channel"]
+            applications, bundle_data["applications"][applications]["channel"], ubuntu_version
         )
 
-    with open(bundle_path, 'w') as bundle:
+    with open(bundle_path, "w") as bundle:
         yaml.dump(bundle_data, bundle)
         bundle.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     update_bundle(sys.argv[1])
