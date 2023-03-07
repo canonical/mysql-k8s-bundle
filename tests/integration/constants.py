8a34808799176e01229ad5cec2838f4477872d9a
Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
2023-03-07 10:51:15 +0100

    Switch to mysql-test-app and improve the test
    Do not fail on 5 minutes timeout if mysql cluster is slow to start on GH.

--- tests/integration/constants.py
+++ tests/integration/constants.py
@@ -3,5 +3,5 @@
 
 MYSQL_APP = "mysql-k8s"
 ROUTER_APP = "mysql-router-k8s"
-APPLICATION_APP = "application"
+APPLICATION_APP = "mysql-test-app"
 TLS_APP = "tls-certificates-operator"
--- tests/integration/test_bundle.py
+++ tests/integration/test_bundle.py
@@ -29,18 +29,16 @@ logger = logging.getLogger(__name__)
 async def test_deploy_bundle(ops_test: OpsTest) -> None:
     """Deploy bundle."""
     async with ops_test.fast_forward():
+        logger.info(f"Deploy MySQL K8s bundle and wait for up and running")
         await ops_test.model.deploy("./releases/latest/mysql-k8s-bundle.yaml", trust=True)
-
-        await ops_test.model.wait_for_idle(apps=[MYSQL_APP], status="active", timeout=15 * 60)
-
         await ops_test.model.wait_for_idle(apps=[ROUTER_APP], status="waiting", timeout=5 * 60)
+        await ops_test.model.wait_for_idle(apps=[MYSQL_APP], status="active", timeout=15 * 60)
 
 
 async def test_mysql_primary_switchover(ops_test: OpsTest):
     """Ensures writes continue after the primary is killed."""
-    # Deploy application & relate to router
-    application_charm = await ops_test.build_charm("./tests/integration/application_charm/")
-    await ops_test.model.deploy(application_charm, application_name=APPLICATION_APP, num_units=1)
+    logger.info(f"Deploy {APPLICATION_APP} & relate to {ROUTER_APP}")
+    await ops_test.model.deploy(APPLICATION_APP)
     await ops_test.model.relate(f"{APPLICATION_APP}:database", f"{ROUTER_APP}:database")
     await ops_test.model.wait_for_idle(
         apps=[MYSQL_APP, ROUTER_APP, APPLICATION_APP],
@@ -48,12 +46,12 @@ async def test_mysql_primary_switchover(ops_test: OpsTest):
         raise_on_blocked=True,
         timeout=15 * 60,
     )
-    # Start writes
+    logger.info("Start continuous writes")
     await ensure_all_units_continuous_writes_incrementing(ops_test)
-    # Kill primary mysql
+    logger.info("Killing primary mysql")
     mysql_units = ops_test.model.applications[MYSQL_APP].units
     primary = await get_primary_unit(ops_test, mysql_units[0], MYSQL_APP)
-    # ensure all units in the cluster are online
+    logger.info("Ensure all units in the cluster are online")
     assert await ensure_n_online_mysql_members(
         ops_test, 3
     ), "The deployed mysql application is not fully online"
@@ -70,14 +68,14 @@ async def test_mysql_primary_switchover(ops_test: OpsTest):
         "SIGKILL",
     )
 
-    # Wait for the SIGKILL above to take effect before continuing with test checks
+    logger.info("Wait for the SIGKILL above to take effect before continuing with checks")
     time.sleep(10)
 
     assert await ensure_n_online_mysql_members(
         ops_test, 3
     ), "The mysql application is not fully online after sending SIGKILL to primary"
 
-    # ensure that the mysqld process got restarted and has a new process id
+    logger.info("Ensure the mysqld process got restarted and has a new process id")
     new_mysql_pid = await get_process_pid(
         ops_test, primary.name, MYSQL_CONTAINER_NAME, MYSQLD_PROCESS_NAME
     )
@@ -90,10 +88,11 @@ async def test_mysql_primary_switchover(ops_test: OpsTest):
         primary.name != new_primary.name
     ), "The mysql primary has not been reelected after sending a SIGKILL"
 
-    # Ensure writes continue on all units except old primary
+    logger.info("Ensure writes continue on all units except old primary")
     mysql_units = [unit for unit in mysql_units if unit.name != primary.name]
     await ensure_all_units_continuous_writes_incrementing(ops_test, mysql_units)
-    # Ensure writes continue on all units
+
+logger.info("Ensure writes continue on all units")
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(
             apps=[MYSQL_APP, ROUTER_APP, APPLICATION_APP],
