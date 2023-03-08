5412c620082d3fde3f4e83f03f06c501489624a1
Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
2023-03-08 18:27:20 +0100

    Switch to mysql-test-app and improve the test (#30)
    * Switch to mysql-test-app and improve the test
    
    Do not fail on 5 minutes timeout if mysql cluster is slow to start on GH.
    
    * fix lint
    
    * Disable caching as no more charm to build here
    
    Also switch to Juju 2.9.38 as we are not scaling down app.
    
    * Remove caching leftovers and unnecessary pinning
    
    * specify channel for test app (edge only for now)
    
    * Bump Juju agent to the latest 2.9.42

--- tests/integration/constants.py
+++ tests/integration/constants.py
@@ -1,7 +1,7 @@
-# Copyright 2022 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 MYSQL_APP = "mysql-k8s"
 ROUTER_APP = "mysql-router-k8s"
-APPLICATION_APP = "application"
+APPLICATION_APP = "mysql-test-app"
 TLS_APP = "tls-certificates-operator"
