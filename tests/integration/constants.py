7e4cd3977d0ab55e2d946abd8e56e6a9911c618e
shayancanonical <99665202+shayancanonical@users.noreply.github.com>
2024-07-15 16:12:05 -0400

    [DPE-4794] Update bundle with all components in the mysql ecosystem + add smoke tests (#68)
    * Update bundle with all components in the mysql ecosystem + add smoke tests
    
    * Update agent version to 3.5.2
    
    * Downgrade to juju 3.4.3
    
    * Specify a strict channel for microk8s

--- tests/integration/constants.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2023 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-MYSQL_APP = "mysql-k8s"
-ROUTER_APP = "mysql-router-k8s"
-APPLICATION_APP = "mysql-test-app"
-TLS_APP = "tls-certificates-operator"
